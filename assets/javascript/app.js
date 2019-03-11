64ad61484c942affe8b5dd22021f32e2932a54e5
jrbricafort <josephricafort1@gmail.com>
2019-03-10 22:55:46 -0400

    added timer to alert player when time is up. 30 seconds provided

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+// this.val
+// setTimeout(fiveSeconds, 1000 * 5);
+// setTimeout(tenSeconds, 1000 * 10);
+setTimeout(timeUp, 1000 * 30);
+
+// function fiveSeconds() {
+//   // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
+//   // console log 10 seconds left
+//   $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
+//   console.log("10 seconds left");
+// }
+
+// function tenSeconds() {
+//   // in the element with an id of time-left add an h2 saying About 5 Seconds Left!
+//   // console log 5 seconds left
+//   $("#time-left").append("<h2>About 5 Seconds Left!</h2>");
+//   console.log("5 seconds left");
+// }
+
+function timeUp() {
+  // in the element with an id of time-left add an h2 saying Time's Up!
+  // console log done
+  console.log("done");
+  $("#time-left").append("<h2>Time's Up!</h2>");
+  alert("Time is up");
+}
