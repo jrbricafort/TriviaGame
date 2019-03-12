46c949060b0d01ce0e7c04fbb57316eb69a81180
jrbricafort <josephricafort1@gmail.com>
2019-03-12 18:29:41 -0400

    final submit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,62 @@
-$(document).ready(function () {
-    console.log("Ready")
-});
+function gamestart() {
+    $("body > form").show()
+    $("#start").hide()
+    $(document).ready(function () {
+        console.log("Ready")
+    });
 
-var right = 0;
-var wrong = 0;
+    var right = 0;
+    var wrong = 0;
 
-$("#righthtml").text(right);
-$("#wronghtml").text(wrong);
+    $("#righthtml").text(right);
+    $("#wronghtml").text(wrong);
 
-$(".answer").on("click", function () {
-    if ($(this).val() == "correct") {
-        right++;
-        console.log(right)
+
+    $(".answer").on("click", function () {
+        if ($(this).val() == "correct") {
+            right++;
+            console.log(right)
+        }
+        else {
+            wrong++;
+            console.log(wrong)
+        }
+    })
+
+
+    $(".submit").on("click", function () {
+        // window.location.href = './resultspage.html';
+        //look for end timer function on redirect or button click
+        clearInterval(interval);
+        // tally();
+        timeUp();
+    })
+
+    setTimeout(timeUp, 1000 * 30);
+
+    function timeUp() {
+        console.log("done");
+        $("#time-left").append("<h2>Time's Up!</h2>");
+        clearInterval(interval);
+        tally();
+        alert("Time is up! " + "You got " + right + " correct and " + wrong + " wrong and " + (5 - right - wrong) + " incomplete.");
     }
-    else {
-        wrong++;
-        console.log(wrong)
+
+    var seconds_left = 30;
+    var interval = setInterval(function () {
+        document.getElementById('timer_div').innerHTML = --seconds_left;
+
+        // if (seconds_left <= 0) {
+        // document.getElementById('timer_div').innerHTML = "You got " + right + " correct and " + wrong + " wrong and " + (5 - right - wrong) + " incomplete.";
+        // }
+    }, 1000);
+
+    function tally() {
+        document.getElementById('timer_div').innerHTML = "You got " + right + " correct and " + wrong + " wrong and " + (5 - right - wrong) + " incomplete.";
     }
-})
-
-$(".submit").on("click", function () {
-    window.location.href = './resultspage.html';
-    //look for end timer function on redirect or button click
-    clearTimeout(timeUp);
-})
-
-setTimeout(timeUp, 1000 * 30);
-
-function timeUp() {
-    console.log("done");
-    $("#time-left").append("<h2>Time's Up!</h2>");
-    alert("Time is up! " + "You got " + right + " correct and " + wrong + " wrong and " + (5-right-wrong) + " incomplete.");
 }
 
-var seconds_left = 30;
-var interval = setInterval(function () {
-    document.getElementById('timer_div').innerHTML = --seconds_left;
-
-    if (seconds_left <= 0) {
-        document.getElementById('timer_div').innerHTML = "You got " + right + " correct and " + wrong + " wrong and " + (5-right-wrong) + " incomplete.";
-        clearInterval(interval);
-    }
-}, 1000);
+$("body > form").hide()
+$("#start").on("click", function () {
+    gamestart();
+})
